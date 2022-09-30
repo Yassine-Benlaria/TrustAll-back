44c06c9c7f6d5d7775b122aeb496b5a6fd788cba
Yassine-Benlaria <ay.benlaria@esi-sba.dz>
2022-09-30 09:13:15 +0100

    creating schemas

---
+++ controllers/client.js
@@ -0,0 +1,5 @@
+const Client = require("../models/client")
+
+exports.signup = (req, res) => {
+    res.json("it's working")
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const expressValidator = require("express-validator")
+const cors = require("cors");
+
+
+require("dotenv").config();
+
+//import routes
+//const patientRoutes = require("./routes/patient")
+
+
+
+
+//app
+const app = express();
+
+
+//db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+}).then(() => console.log("DataBase Connected!!"));
+
+//middlwares
+app.use(cors());
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+
+
+//routes middlware
+//app.use("/api/patient", patientRoutes);
+
+
+
+
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
