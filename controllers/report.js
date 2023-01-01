208f1d1de130a37e4c32a4c88b13dd91168c7afc
Yassine-Benlaria <ay.benlaria@esi-sba.dz>
2023-01-01 14:47:10 +0100

    f4

--- controllers/report.js
+++ controllers/report.js
@@ -19,59 +19,68 @@ exports.uploadImages = (req, res) => {
 }
 
 exports.createReport = (req, res) => {
-    console.log("req:---", req)
-    console.log("body:---", req.body)
-    console.log("files:---", req.files)
-    Command.findById(req.body.command_id, (err, command) => {
-        //if command not found
-        if (err || !command) return res.status(400).json({ err: "Command not found!" })
+    imagesUpload(req, res, (err) => {
+        if (err) return res.status(400).json({ err })
 
-        //if command found
-        Plan.findById(command.plan_id, (err, plan) => {
-            if (err || !plan) return res.status(400).json({ err: "Plan not found!" })
-            let report_json = {
-                created_by: req.params.id,
-                command_id: req.body.command_id,
-                car_information: {},
-                interior: {},
-                exterior: {},
-                mechanical: {}
-            };
-            //car information
-            plan.car_information.forEach(element => {
-                report_json.car_information[element] = req.body.car_information[element]
+
+        console.log("body:---", req.body)
+        console.log("files:---", req.files)
+            // console.log("req:---", req)
+        Command.findById(req.body.command_id, (err, command) => {
+
+            // uploadFilesToImageKit(req.files)
+            // return res.send("images uploaded successfully")
+            //if command not found
+            if (err || !command) return res.status(400).json({ err: "Command not found!" })
+
+            //if command found
+            Plan.findById(command.plan_id, (err, plan) => {
+                if (err || !plan) return res.status(400).json({ err: "Plan not found!" })
+                let report_json = {
+                    created_by: req.params.id,
+                    command_id: req.body.command_id,
+                    car_information: {},
+                    interior: {},
+                    exterior: {},
+                    mechanical: {}
+                };
+                //car information
+                plan.car_information.forEach(element => {
+                    report_json.car_information[element] = req.body.car_information[element]
+                });
+
+                //interior
+                plan.interior.forEach(element => {
+                    report_json.interior[element] = {
+                        description: req.body.interior[element],
+                        status: req.body["interior_check"][element] == "on" ? true : false
+                    }
+
+                });
+
+                //exterior
+                plan.exterior.forEach(element => {
+                    report_json.exterior[element] = {
+                        description: req.body.exterior[element],
+                        status: req.body["exterior_check"][element] == "on" ? true : false
+                    }
+                });
+
+                //mechanical
+                plan.mechanical.forEach(element => {
+                    report_json.mechanical[element] = {
+                        description: req.body.mechanical[element],
+                        status: req.body["mechanical_check"][element] == "on" ? true : false
+                    }
+                });
+
+                console.log("report_json", report_json);
+
+
+                report = new Report(report_json)
+                report.save()
+                res.json({ msg: "normalement everything is ok" })
             });
-
-            //interior
-            plan.interior.forEach(element => {
-                report_json.interior[element] = {
-                    description: req.body.interior[element],
-                    status: req.body["interior_check"][element] == "on" ? true : false
-                }
-
-            });
-
-            //exterior
-            plan.exterior.forEach(element => {
-                report_json.exterior[element] = {
-                    description: req.body.exterior[element],
-                    status: req.body["exterior_check"][element] == "on" ? true : false
-                }
-            });
-
-            //mechanical
-            plan.mechanical.forEach(element => {
-                report_json.mechanical[element] = {
-                    description: req.body.mechanical[element],
-                    status: req.body["mechanical_check"][element] == "on" ? true : false
-                }
-            });
-
-            console.log("report_json", report_json);
-
-            report = new Report(report_json)
-            report.save()
-            res.json({ msg: "normalement everything is ok" })
 
         })
     });
