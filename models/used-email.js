e1a2e984d12aaac5f7f9b4225facc610331b7e05
Yassine-Benlaria <ay.benlaria@esi-sba.dz>
2023-01-04 16:24:52 +0100

    ff

--- models/used-email.js
+++ models/used-email.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const usedEmailSchema = new mongoose.Schema({
     email: { type: String, required: true }
 }, {
-    timestamps: false,
-    _id: false
+    timestamps: false
 });
 
 module.exports = mongoose.model("UsedEmail", usedEmailSchema);
