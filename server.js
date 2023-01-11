3beb1443327b089267f8a93a95c8f849df32f07b
Yassine-Benlaria <benlariay@gmail.com>
2023-01-11 22:20:22 +0100

    testing

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ mongoose.connect(process.env.DATABASE, {
 //middlwares
 app.use(cors());
 app.use(morgan("dev"))
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
 
