6b25027596a4323e5c07f91a5c5d5c7bc3527a92
Yassine-Benlaria <benlariay@gmail.com>
2023-01-29 23:33:55 +0100

    fixing errors

--- controllers/settings.js
+++ controllers/settings.js
@@ -16,18 +16,22 @@ exports.createSettings = async(req, res) => {
 exports.updateSettings = (req, res) => {
     console.log(req.body)
 
-    let json = { social_media: {}, location: {} };
+    let json = {};
     if (req.body.facebook != "") json.social_media.facebook = req.body.facebook;
     if (req.body.instagram != "") json.social_media.instagram = req.body.instagram;
     if (req.body.twitter != "") json.social_media.twitter = req.body.twitter;
     if (req.body.whatsapp != "") json.social_media.whatsapp = req.body.whatsapp;
     if (req.body.email != "") json.social_media.email = req.body.email;
-    if (req.body.terms != "") json.terms = req.body.terms;
+    if (req.body.terms) {
+        if (req.body.terms.ar) json["terms.ar"] = req.body.terms.ar;
+        if (req.body.terms.fr) json["terms.fr"] = req.body.terms.fr;
+        if (req.body.terms.en) json["terms.en"] = req.body.terms.en;
+    }
 
     if (req.body.location) {
-        if (req.body.location.ar) json.location.ar = req.body.location.ar;
-        if (req.body.location.fr) json.location.fr = req.body.location.fr;
-        if (req.body.location.en) json.location.en = req.body.location.en;
+        if (req.body.location.ar) json["location.ar"] = req.body.location.ar;
+        if (req.body.location.fr) json["location.fr"] = req.body.location.fr;
+        if (req.body.location.en) json["location.en"] = req.body.location.en;
     }
 
     if (req.body.faqs) json.FAQs = req.body.faqs;
